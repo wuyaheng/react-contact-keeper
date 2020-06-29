165b81725aaef689502ebba9707b59bbd94f94cb
wuyaheng <wuyaheng2016@gmail.com>
2020-06-28 20:19:57 -0400

    add route protection

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-
+//Connect Database
 connectDB();
 
-// Init Middleware
+//Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => 
-    res.json({msg: "Welcome to the Contact Keeper API..."}))
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 
-
+//Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
