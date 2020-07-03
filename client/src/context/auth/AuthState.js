2a7eb550435a681225f5a03abcc7474c26bdd9bb
wuyaheng <wuyaheng2016@gmail.com>
2020-07-03 16:19:19 -0400

    add token

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken'; 
 
 import {
     REGISTER_SUCCESS,
@@ -27,19 +29,56 @@ const AuthState = props => {
 
 
     // Load User
+    const loadUser = async () => {
+        if(localStorage.token) {
+            setAuthToken(localStorage.token)
+        }
+        try {
+            const res = await axios.get('./api/auth');
+            dispatch({
+                type: USER_LOADED,
+                payload: res.data
+            });
+            }
+            catch (err) {
+                dispatch({type: AUTH_ERROR});
+            }
+    }
 
 
     // Register User
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/users',formData,config);
+
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            });
+            loadUser();
+        } catch (err) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            })
+        }
+    }
 
 
     // Login User
-
+    const login = () => console.log('login');
 
     // Logout
-
+    const logout = () => console.log('logout');
 
     // Clear Errors
-
+    const clearErrors = () => dispatch({type: CLEAR_ERRORS});
 
     return (
         <AuthContext.Provider
@@ -48,7 +87,12 @@ const AuthState = props => {
                 isAuthenticated: state.isAuthenticated,
                 loading: state.loading,
                 user: state.user,
-                error: state.error
+                error: state.error,
+                register,
+                loadUser,
+                login,
+                logout,
+                clearErrors
             }}   
             >
                 {props.children}
