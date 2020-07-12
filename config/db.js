9a9544403f001e299017a6d36823a26000c16399
wuyaheng <wuyaheng2016@gmail.com>
2020-07-11 21:42:59 -0400

    delete deprecated

--- config/db.js
+++ config/db.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-
 const connectDB = async () => {
     try {
         await mongoose
         .connect(db, {
+            useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
-            useFindAndModify: false
+            useFindAndModify:false
         });
-        console.log("MongoDB Connected...")
-    } catch(err) {
-        console.log(err.message)
-        process.exit(1);
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1)
     }
 }
-
 module.exports = connectDB;
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config')
-const { check, validationResult } = require('express-validator/check');
-
+//const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
-
 //@route  POST api/users
 //@desc   Register a user
 //acccess Public
@@ -20,33 +19,26 @@ router.post('/', [
         return res.status(400).json({ errors: errors.array() })
     }
     const { name, email, password } = req.body;
-
     try {
         let user = await User.findOne({ email });
-
         if(user) {
             return res.status(400).json({msg: "User already exists"})
         }
-
         user = new User({
             name,
             email,
             password
         });
-
         const salt = await bcrypt.genSalt(10);
-
         user.password = await bcrypt.hash(password, salt);
-
         await user.save();
-
         const payload = {
             user: {
                 id: user.id
             }
         }
-
-        jwt.sign(payload, config.get("jwtSecret"), {
+        jwt.sign(payload, config.get("jwtSecret"),
+        {
             expiresIn: 360000
         }, (err, token) => {
             if(err) throw err;
@@ -57,5 +49,4 @@ router.post('/', [
         res.status(500).send("Server Error")
     }
 });
-
 module.exports = router;
